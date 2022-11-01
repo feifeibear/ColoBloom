90e3466f39d222ac619f7f5082c6ac9b08f2bc36
BlueRum <70618399+ht-zhou@users.noreply.github.com>
2022-11-01 09:34:02 +0800

    quantize -> colo_init

--- bloom-inference-scripts/bloom_basic.py
+++ bloom-inference-scripts/bloom_basic.py
@@ -4,8 +4,77 @@ import torch.distributed as dist
 from colossalai.tensor import ProcessGroup
 
 import colossalai
-from colossalai.utils.model.colo_init_context import ColoInitContext
-from colossalai.tensor import ShardSpec, ComputeSpec, ComputePattern, ColoParameter, ProcessGroup
+from colossalai.tensor import ShardSpec, ComputeSpec, ComputePattern, ColoInt8Parameter, ProcessGroup, ColoTensor, ColoParameter
+
+from torch import nn
+from typing import Iterator, Tuple, Union
+from bitsandbytes.nn.modules import Linear8bitLt
+
+
+def _named_params_with_replica(
+    module: nn.Module,
+    prefix: str = '',
+    recurse: bool = True,
+) -> Iterator[Tuple[str, Union[nn.Parameter, ColoTensor]]]:
+    modules = module.named_modules(prefix=prefix) if recurse else [(prefix, module)]
+
+    for mod_prefix, mod in modules:
+        for name, val in mod._parameters.items():
+            if val is None:
+                continue
+            name = mod_prefix + ('.' if mod_prefix else '') + name
+            yield name, val
+
+def ColoModulize(module):
+    """
+    Replacing the parameters() and named_parameters() with our customized ones
+    """
+
+    module._colo_visited = True
+
+def to_colo_tensor(module):
+    name_list = []
+    for name, param in _named_params_with_replica(module):
+        if isinstance(param, ColoTensor):
+            continue
+
+        split = name.rfind('.')
+        if split >= 0:    # param in submodule
+            module_name = name[:split]
+            param_name = name[split + 1:]
+        else:
+            module_name = ''    # param in current module
+            param_name = name
+        name_list.append((module_name, param_name))   
+    
+    replaced_tensors = dict(
+    )    # record mapping between (torch.Tensor, ColoTensor) to distinguish the same reference
+    for module_name, param_name in name_list:
+        submodule = module.get_submodule(module_name)
+        if isinstance(submodule, Linear8bitLt):
+            param = submodule.get_parameter(param_name)
+            # print(param)
+            if param in replaced_tensors:
+                colo_param = replaced_tensors[param]
+            else:
+                colo_param = ColoInt8Parameter(param, requires_grad=False)
+                # add mapping record
+                replaced_tensors[param] = colo_param
+        else:
+            param = submodule.get_parameter(param_name)
+            if param in replaced_tensors:
+                colo_param = replaced_tensors[param]
+            else:
+                colo_param = ColoParameter(param, requires_grad=False)
+                # add mapping record
+                replaced_tensors[param] = colo_param
+        delattr(submodule, param_name)
+        setattr(submodule, param_name, colo_param)
+        colo_param.shared_param_modules.append(submodule)
+
+    ColoModulize(module)
+    return module
+
 
 
 def run_torch():
@@ -15,6 +84,7 @@ def run_torch():
     kwargs["load_in_8bit"] = True
     tokenizer = BloomTokenizerFast.from_pretrained("/data2/users/lczht/bloom-560m")
     model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m", **kwargs)
+    # model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m").cuda()
     inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
 
     for k, v in inputs.items():
@@ -25,35 +95,46 @@ def run_torch():
     loss = outputs.loss
     logits = outputs.logits
     print(logits)
-    print(model.device)
+
+
 
 def run_CAI():
     colossalai.launch_from_torch(config={})
 
     tokenizer = BloomTokenizerFast.from_pretrained("/data2/users/lczht/bloom-560m")
 
-    with ColoInitContext(device=torch.device('cuda')):
-        model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m")
+    kwargs = dict(
+        device_map='balanced_low_0'
+    )
+    kwargs["load_in_8bit"] = True
 
+    
+    model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m", **kwargs)
+    # model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m")
+
+    model = to_colo_tensor(model)
+
+    print(model)
+    # print(model)
     inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
 
     for k, v in inputs.items():
         inputs[k] = v.cuda()
 
-    for name, p in model.named_parameters():
-        print(name)
+    # for name, p in model.named_parameters():
+    #     print(name)
 
     # add ColossalAI Tensor Splitting Parallel
-    def split_param_single_dim_tp1d(dim: int, param: ColoParameter, pg: ProcessGroup):
+    def split_param_single_dim_tp1d(dim: int, param: ColoInt8Parameter, pg: ProcessGroup):
         spec = (ShardSpec([dim], [pg.tp_world_size()]), ComputeSpec(ComputePattern.TP1D))
         if param.process_group.tp_world_size() == 1:
             param.set_process_group(pg)
         param.set_tensor_spec(*spec)
 
-    def split_param_row_tp1d(param: ColoParameter, pg: ProcessGroup):
+    def split_param_row_tp1d(param: ColoInt8Parameter, pg: ProcessGroup):
         split_param_single_dim_tp1d(0, param, pg)
 
-    def split_param_col_tp1d(param: ColoParameter, pg: ProcessGroup):
+    def split_param_col_tp1d(param: ColoInt8Parameter, pg: ProcessGroup):
         split_param_single_dim_tp1d(-1, param, pg)
 
     pg = ProcessGroup(tp_degree=dist.get_world_size())
@@ -74,4 +155,4 @@ def run_CAI():
     print(logits)
 
 if __name__ == '__main__':
-    run_torch()
+    run_CAI()
