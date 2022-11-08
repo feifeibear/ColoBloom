9cd05576ee9e39b4cf8bd655d799455d8248697e
BlueRum <70618399+ht-zhou@users.noreply.github.com>
2022-11-08 18:09:14 +0800

    Update bloom_int8_tp.py

--- bloom-inference-scripts/bloom_int8_tp.py
+++ bloom-inference-scripts/bloom_int8_tp.py
@@ -6,7 +6,10 @@ from torch import Tensor, device, dtype, nn
 import bitsandbytes as bnb
 import os
 from typing import Optional, TypeVar, Union, overload
+import numpy as np
 
+# random_seed
+torch.manual_seed(0)
 
 T = TypeVar("T", bound="torch.nn.Module")
 class Int8Params(torch.nn.Parameter):
@@ -39,15 +42,17 @@ class Linear8bitLt(nn.Linear):
         bias=True,
         has_fp16_weights=False,
         memory_efficient_backward=False,
-        threshold=0.0,
+        threshold=6.0,
+        weight_data=None,
         index=None,
+        bias_data=None
     ):
         super(Linear8bitLt, self).__init__(
             input_features, output_features, bias
         )
         self.state = bnb.MatmulLtState()
         self.index = index
-
+        self.bias = bias_data
         self.state.threshold = threshold
         self.state.has_fp16_weights = has_fp16_weights
         self.state.memory_efficient_backward = memory_efficient_backward
@@ -57,7 +62,7 @@ class Linear8bitLt(nn.Linear):
         self.rank = dist.get_rank()
         self.world_size = dist.get_world_size()
 
-        weight = self.weight.contiguous().half().to(self.rank)
+        weight = weight_data.data.contiguous().half().to(self.rank)
         CB, _, SCB, _, _ = bnb.functional.double_quant(weight)
         self.weight = Int8Params(data=CB, SCB=SCB)
 
@@ -65,21 +70,35 @@ class Linear8bitLt(nn.Linear):
     def forward(self, x):
         self.state.is_training = self.training
         
-        
         # weights are cast automatically as Int8Params, but the bias has to be cast manually
         if self.bias is not None and self.bias.dtype != torch.float16:
             self.bias.data = self.bias.data.half()
         
         self.state.CB = self.weight.data
         self.state.SCB = self.weight.SCB
-        out = bnb.matmul(x, self.weight, bias=self.bias, state=self.state)
 
+        out = bnb.matmul(x, self.weight.data, bias=self.bias, state=self.state)
         tensor_list = [torch.zeros_like(out) for _ in range(self.world_size)]
         dist.all_gather(tensor_list, out)
         out = torch.cat(tensor_list, dim=2)
-        # out = torch.cat(tensor_list, dim=1)
 
         return out
+
+def replace_8bit_linear(model, threshold=6.0, modules_to_not_convert="lm_head"):
+    for name, module in model.named_children():
+        if len(list(module.children())) > 0:
+            replace_8bit_linear(module, threshold, modules_to_not_convert)
+
+        if isinstance(module, nn.Linear) and name not in modules_to_not_convert:
+                model._modules[name] = Linear8bitLt(
+                        input_features=module.in_features,
+                        output_features=module.out_features,
+                        threshold=6.0,
+                        weight_data=module.weight,
+                        bias_data=module.bias,
+                )
+    return model
+
 
 def run_tp():
     # init
@@ -89,20 +108,6 @@ def run_tp():
     dist.init_process_group(backend='nccl', world_size=world_size, rank=rank)
 
     model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m")
-    
-    # quantization
-    def replace_8bit_linear(model, threshold=6.0, modules_to_not_convert="lm_head"):
-        for name, module in model.named_children():
-            if len(list(module.children())) > 0:
-                replace_8bit_linear(module, threshold, modules_to_not_convert)
-
-            if isinstance(module, nn.Linear) and name not in modules_to_not_convert:
-                    model._modules[name] = Linear8bitLt(
-                        input_features=module.in_features,
-                        output_features=module.out_features
-                    )
-        return model
-    
     model = replace_8bit_linear(model).to(rank)
 
     # TP
@@ -115,31 +120,46 @@ def run_tp():
             SCB = SCB_list[rank]
             module.weight = Int8Params(data=weight, SCB=SCB)
 
+            bias_list = list(module.bias.data.chunk(world_size, dim=0))
+            bias = bias_list[rank]
+            module.bias = nn.Parameter(bias)
+
     # inputs
     tokenizer = BloomTokenizerFast.from_pretrained("/data2/users/lczht/bloom-560m")
     inputs = tokenizer("Hello, my dog is cute", return_tensors="pt").to(rank)
-    for k, v in inputs.items():
-        inputs[k] = v
     
     # inference
-    outputs = model(**inputs, labels=inputs["input_ids"])
-    loss = outputs.loss
-    logits = outputs.logits
-    print(logits)
+    
+    # outputs = model(**inputs, labels=inputs["input_ids"])
+    # loss = outputs.loss
+    # logits = outputs.logits
+    # print(logits)
+    
+    outputs = model.generate(**inputs, labels=inputs["input_ids"])
+    output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    print(outputs)
+    print(output)
 
 
 def run_ori():
-    model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m").to("cuda:0")
-    tokenizer = BloomTokenizerFast.from_pretrained("/data2/users/lczht/bloom-560m")
-    inputs = tokenizer("Hello, my dog is cute", return_tensors="pt").to("cuda:0")
-    for k, v in inputs.items():
-        inputs[k] = v
+    model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m", load_in_8bit=True)
     
+    # quantization
+    model = replace_8bit_linear(model).to(0)
+
+    tokenizer = BloomTokenizerFast.from_pretrained("/data2/users/lczht/bloom-560m")
+    inputs = tokenizer("Hello, my dog is cute", return_tensors="pt").to(0)
+    
+    # outputs = model.generate(**inputs, labels=inputs["input_ids"])
+    # output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     outputs = model(**inputs, labels=inputs["input_ids"])
-    loss = outputs.loss
-    logits = outputs.logits
+    output = outputs.logits
+    out = output[0].detach().cpu().numpy()
+    outputpath = 'ori.csv'
+    np.savetxt(outputpath, out, fmt='%.4f',delimiter=',')
+    print(output.shape)
 
-    print(logits)
-
+       
 if __name__ == '__main__':
     run_tp()
+    # run_ori()
