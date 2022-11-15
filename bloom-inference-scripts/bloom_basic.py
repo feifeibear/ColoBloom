f12fb8f7d787cb34dfb23af0194f42052600921c
jiaruifang <fangjiarui123@gmail.com>
2022-11-15 11:12:14 +0800

    [dev] add shard init for fp32 TP

--- bloom-inference-scripts/bloom_basic.py
+++ bloom-inference-scripts/bloom_basic.py
@@ -1,11 +1,11 @@
 import torch
-from transformers import BloomTokenizerFast, BloomForCausalLM
+from transformers import BloomTokenizerFast, BloomForCausalLM, BloomConfig
 import torch.distributed as dist
 from colossalai.tensor import ProcessGroup
 
 import colossalai
 from colossalai.utils.model.colo_init_context import ColoInitContext
-from colossalai.tensor import ShardSpec, ComputeSpec, ComputePattern, ColoParameter, ProcessGroup
+from colossalai.tensor import ShardSpec, ComputeSpec, ComputePattern, ColoParameter, ProcessGroup, ReplicaSpec
 
 
 def run_torch():
@@ -32,24 +32,27 @@ def run_torch():
 
 def run_CAI():
     colossalai.launch_from_torch(config={})
+    world_size = dist.get_world_size()
+    pg = ProcessGroup(tp_degree=world_size)
 
     tokenizer = BloomTokenizerFast.from_pretrained("/data2/users/lczht/bloom-560m")
 
-    with ColoInitContext(device=torch.device('cuda')):
-        model = BloomForCausalLM.from_pretrained("/data2/users/lczht/bloom-560m")
+    cfg = BloomConfig()
+    default_shard_plan = {'pg' : pg, 'shard_spec' : ShardSpec([-1], [world_size])}
+    with ColoInitContext(device=torch.device('cuda'), default_shard_plan = default_shard_plan):
+        model = BloomForCausalLM(cfg)
+        #from_pretrained("/data2/users/lczht/bloom-560m")
 
     inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
 
     for k, v in inputs.items():
         inputs[k] = v.cuda()
 
-
-
     # add ColossalAI Tensor Splitting Parallel
     def split_param_single_dim_tp1d(dim: int, param: ColoParameter, pg: ProcessGroup):
         spec = (ShardSpec([dim], [pg.tp_world_size()]), ComputeSpec(ComputePattern.TP1D))
-        if param.process_group.tp_world_size() == 1:
-            param.set_process_group(pg)
+        # if param.process_group.tp_world_size() == 1:
+        #     param.set_process_group(pg)
         param.set_tensor_spec(*spec)
 
     def split_param_row_tp1d(param: ColoParameter, pg: ProcessGroup):
@@ -58,26 +61,30 @@ def run_CAI():
     def split_param_col_tp1d(param: ColoParameter, pg: ProcessGroup):
         split_param_single_dim_tp1d(-1, param, pg)
 
-    pg = ProcessGroup(tp_degree=dist.get_world_size())
+    shard_param_names = ['self_attention.dense.weight', 'dense_h_to_4h.weight', 'dense_4h_to_h.weight', 'self_attention.query_key_value.weight', 'word_embeddings.weight']
     for mn, module in model.named_modules():
         for pn, param in module.named_parameters(recurse=False):
             # reset process group for all parameters
-            param.set_process_group(pg)
+            # param.set_process_group(pg)
+            if hasattr(param, 'is_visited'):
+                continue
             param_name = f"{mn}.{pn}"
-
-            shard_param_names = ['self_attention.dense.weight', 'dense_h_to_4h.weight', 'dense_4h_to_h.weight', 'self_attention.query_key_value.weight', 'word_embeddings.weight']
-            is_shard = False
+            
+            use_shard = False
             for keyword in shard_param_names:
                 if keyword in param_name:
                     split_param_col_tp1d(param, pg)  # colmn slice 
-                    # print(f'split_param_row_tp1d for {param_name}')
-                    is_shard = True
+                    print('col slice', param_name)
+                    use_shard = True
+                    # replicated param
+            if not use_shard:
+                param.set_dist_spec(ReplicaSpec())
             
-            # if not is_shard and 'bias' not in param_name:
-            #     print(param_name)
+            param.is_visited = True
 
     total_numel = 0
     for name, p in model.named_parameters():
+        # print(name, p.dist_spec.placement.value)
         total_numel += p.numel()
     print(f"numel of the model {total_numel/1e9}")
 
